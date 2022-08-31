3e5fcbb61689d8496c1037fdb449c4a2d1f2e753
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2022-08-31 12:01:36 +0800

    Create AVL_Tree.java

---
+++ AVL-Tree/src/algorithm/AVL_Tree.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Frank.Ren
+ * @version : 1.0
+ * @Project : DataStructureProgress
+ * @Package : algorithm
+ * @createTime : 2022/8/31 12:01
+ * @Email : sc19lr@leeds.ac.uk
+ * @github : https://github.com/frankRenlf
+ * @Description :
+ */
+public class AVL_Tree {
+
+    
+
+}
